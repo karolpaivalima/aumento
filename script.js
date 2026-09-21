9e0ae49b0509d1ff163c7f0bf9587311738ae7e5
Karoline Carvalho Paiva Terceiro Lima <karolinepaivaterceiro@gmail.com>
2026-09-21 18:28:07 -0300

    lógica resolvida

---
+++ script.js
@@ -0,0 +1,26 @@
+const button = document.querySelector("button")
+const salarioInput = document.getElementById("salario")
+const novoSalarioInput = document.getElementById("novo-salario")
+const resultado = document.getElementById("resultado")
+
+button.onclick = calcularAumento
+
+function calcularAumento() {
+    const salario = salarioInput.valueAsNumber
+    const novoSalario = novoSalarioInput.valueAsNumber
+
+    if (
+        salarioInput.value === "" ||
+        novoSalarioInput.value === ""
+    ) {
+        alert("Preencha todos os campos.")
+        return
+    }
+
+    let aumento = novoSalario - salario
+    let porcentagem = ((novoSalario*100)/salario) - 100
+
+
+    resultado.textContent = `Aumento = ${aumento.toFixed(2)} reais
+    Porcentagem = ${porcentagem}%`
+}
